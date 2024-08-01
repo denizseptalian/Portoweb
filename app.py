4e56c67b3f61985a332b308739640f9df7f8dc59
Denisa septalian/21083010113 <106682572+denizseptalian@users.noreply.github.com>
2024-08-02 01:28:37 +0700

    Update app.py 2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Kim Marcial Vallesteros", page_icon=logo, layout=
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("style/style.css")
+local_css("style.css")
 
 
 st.markdown(
